91a523bb21ad50a18c47bf1827034919fd6589a4
Mihai Farcas <farcasmihai91@gmail.com>
2016-07-05 11:32:52 +0300

    Dependencies and Config
    Added new dependencies: angular2, bootstrap etc. (see package.json).
    Configured Webpack (see webpack.config.js).
    Configured TypeScript (see tsconfig.json).

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+/* 
+* Using Webpack to bundle up the Angular 2 scripts and serve them from a single angular2.js bundle 
+* that will be in the build directory. 
+* The scripts for our app will be served from a separate bundle called app.js.
+*/
+
+var path = require('path');
+var webpack = require('webpack');
+var CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin;
+
+module.exports = {
+  devtool: 'source-map',
+  debug: true,
+
+  entry: {
+    'angular2': [
+      'rxjs',
+      'reflect-metadata',
+      'angular2/core',
+      'angular2/router',
+      'angular2/http'
+    ],
+    'app': './app/app'
+  },
+
+  output: {
+    path: __dirname + '/build/',
+    publicPath: 'build/',
+    filename: '[name].js',
+    sourceMapFilename: '[name].js.map',
+    chunkFilename: '[id].chunk.js'
+  },
+
+  resolve: {
+    extensions: ['','.ts','.js','.json', '.css', '.html']
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.ts$/,
+        loader: 'ts',
+        exclude: [ /node_modules/ ]
+      }
+    ]
+  },
+
+  plugins: [
+    new CommonsChunkPlugin({ name: 'angular2', filename: 'angular2.js', minChunks: Infinity }),
+    new CommonsChunkPlugin({ name: 'common',   filename: 'common.js' })
+  ]
+};
